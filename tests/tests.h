7d5ff24eab5d13b0196a62d20106f19fd72f1d3b
BRONTIK <50723771+BRONTIK@users.noreply.github.com>
2019-06-01 08:33:07 +0300

    Update tests.h

--- tests/tests.h
+++ tests/tests.h
@@ -9,19 +9,19 @@
 */ 
 TEST(test1, suite1)
 {
-//    Widget *w = new Widget();
+    Widget *w = new Widget();
 
-//    w->list_filling("");
-//    int count = 0;
-//    QFile file2("/home/02/abramov/sem2/untitled/listItems");
-//    if (!file2.open(QIODevice::ReadOnly | QIODevice::Text))
-//        return;
-//    QTextStream out(&file2);
-//    while (!out.atEnd()) {
-//        QString line = out.readLine();
-//        EXPECT_EQ(line, ui->List->item(count));
-//        count++;
-//    }
+    w->list_filling("");
+    int count = 0;
+    QFile file2("/home/02/abramov/sem2/untitled/listItems");
+    if (!file2.open(QIODevice::ReadOnly | QIODevice::Text))
+        return;
+    QTextStream out(&file2);
+    while (!out.atEnd()) {
+        QString line = out.readLine();
+        EXPECT_EQ(line, ui->List->item(count));
+        count++;
+    }
     SUCCEED();
 }
 /* Тест проверяющий, что в подсказке вылезают нужные элементы
