0e35dc1c7c5d360d0b1c9eea4002dfe9f561ce2a
BRONTIK <50723771+BRONTIK@users.noreply.github.com>
2019-06-02 22:29:30 +0300

    Update tests.h

--- tests/tests.h
+++ tests/tests.h
@@ -26,15 +26,9 @@ TEST(test1, suite1)
         count++;
     }
     
-//    EXPECT_EQ("Corpse reviver", List.at(34));
-//    EXPECT_EQ("Dark n stormy", List.at(33));
-//       EXPECT_EQ("Rum old fashioned", List.at(35));
-//    EXPECT_EQ("Penicillin", List.at(36));
-    
-//      EXPECT_EQ(2, List.count());
-//     EXPECT_EQ(2, count);
     SUCCEED();
 }
+
 /* Тест проверяющий, что в подсказке вылезают нужные элементы
  *  и что регистр не имеет значения
  */
@@ -49,39 +43,35 @@ TEST(test2, suite2)
     
    SUCCEED();
 }
+
 /* Тест проверяющий, что в подсказке вылезают нужные элементы
  *  и что регистр не имеет значения
  */
 TEST(test3, suite3)
 {
-//    Widget *w = new Widget();
-//    w->list_filling("");
-//    ui->lineEdit->setText("Сo");
-//    EXPECT_EQ("Cosmopolitan", ui->List->item(0));
-//    EXPECT_EQ("Corpse reviver", ui->List->item(1));
+    QStringList List;
+    list_filling_malyar(&List, "Co");
+    
+    EXPECT_EQ("Cosmopolitan", List.at(0));
+    EXPECT_EQ("Corpse reviver", List.at(1));
+    
     SUCCEED();
 }
+
 /* Тест проверяющий, что в подсказке вылезают нужные элементы
  *  и что регистр не имеет значения
  */
-//TEST(test4, suite4){
-//    list_filling("");
-//    ui->lineEdit->setText("p");
-//    list_filling("p");
-//    while (count < 5) {
-//        w.addItem(ui->List->item(count));
-//        count++;
-//    }
-//    count = 0;
-//    ui->lineEdit->setText("P");
-//{
-//    QStringList w;
-//    int count = 0;
-//    list_filling("P");
-//    while (count < 5) {
-//        EXPECT_EQ(w.itemText(), ui->List->item(count));
-//        count++;
-//    }
-//}
-//}
-#endif // TESTS_H */
+TEST(test4, suite4){
+    QStringList List, w;
+    list_filling_malyar(&List, "p");
+    list_filling_malyar(&w, "P");
+    
+    int count = 0;
+    
+    while (count < 5) {
+       EXPECT_EQ(w.at(count), List.at(count));
+       count++;
+   }
+}
+}
+#endif // TESTS_H 
