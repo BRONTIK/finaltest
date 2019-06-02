a0fd11d4af1c86a0d2fdd92484c2ee7d789ab09d
BRONTIK <50723771+BRONTIK@users.noreply.github.com>
2019-06-02 18:23:20 +0300

    Update main.cpp

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,13 +1,8 @@
 #include <gtest/gtest.h>
 #include "tests.h"
-#include "widget.h"
-
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    Widget w;// = new Widget();
-    w.show();
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
