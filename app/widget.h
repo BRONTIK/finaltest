00488654f5c5ec6bfc555c0cdb78bcfa6b3efd1e
BRONTIK <50723771+BRONTIK@users.noreply.github.com>
2019-06-02 18:49:35 +0300

    Update widget.h

--- app/widget.h
+++ app/widget.h
@@ -1,52 +1,52 @@
-#ifndef WIDGET_H
-#define WIDGET_H
+// #ifndef WIDGET_H
+// #define WIDGET_H
 
-#include <QWidget>
-#include <string>
-#include <QListWidgetItem>
-#include <QString>
-#include <QStringList>
-#include <QFile>
-#include <QDir>
-#include <QFileDialog>
-#include <QTextStream>
-#include <iostream>
-#include <QListWidget>
-#include <QListWidgetItem>
-#include <QMenuBar>
-#include <QAction>
-#include <cstring>
-#include <fstream>
-#include <QListWidgetItem>
-#include <QFrame>
+// #include <QWidget>
+// #include <string>
+// #include <QListWidgetItem>
+// #include <QString>
+// #include <QStringList>
+// #include <QFile>
+// #include <QDir>
+// #include <QFileDialog>
+// #include <QTextStream>
+// #include <iostream>
+// #include <QListWidget>
+// #include <QListWidgetItem>
+// #include <QMenuBar>
+// #include <QAction>
+// #include <cstring>
+// #include <fstream>
+// #include <QListWidgetItem>
+// #include <QFrame>
 
-namespace Ui {
-class Widget;
-}
+// namespace Ui {
+// class Widget;
+// }
 
-class Widget : public QWidget
-{
-    Q_OBJECT
+// class Widget : public QWidget
+// {
+//     Q_OBJECT
 
-public:
-    Ui::Widget *ui;
-    QListWidget List;
-    void comboBox_filling();
-    void textBrowser_filling(QString name);
-    void list_filling(const QString &arg1);
-    explicit Widget(QWidget *parent = NULL);
-    ~Widget();
+// public:
+//     Ui::Widget *ui;
+//     QListWidget List;
+//     void comboBox_filling();
+//     void textBrowser_filling(QString name);
+//     void list_filling(const QString &arg1);
+//     explicit Widget(QWidget *parent = NULL);
+//     ~Widget();
 
-private slots:
-    void on_comboBox_activated();
+// private slots:
+//     void on_comboBox_activated();
 
-    void on_lineEdit_textChanged(const QString &arg1);
+//     void on_lineEdit_textChanged(const QString &arg1);
 
-    void on_List_itemActivated(QListWidgetItem *item);
+//     void on_List_itemActivated(QListWidgetItem *item);
 
-   // void on_lineEdit_selectionChanged();
+//    // void on_lineEdit_selectionChanged();
 
-};
+// };
 
-#endif // WIDGET_H
+// #endif // WIDGET_H
 
