f734fbe313935e74130679973bc6f3c1bf47ef97
BRONTIK <50723771+BRONTIK@users.noreply.github.com>
2019-06-02 18:18:14 +0300

    Update functions.cpp

---
+++ app/functions.cpp
@@ -0,0 +1,49 @@
+#include "functions.h"
+#include <QFile>
+#include <QTextStream>
+#include <QString>
+
+
+void list_filling_mleat(QStringList *List, QString substring)
+{
+    QFile file("dataBase/fff_1.txt");
+
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
+        return;
+
+    /* Счётчик пробелов */
+    int spaces_counter = 0;
+
+    /* Выбранная категория */
+    QString category;
+
+    QTextStream in(&file);
+    //QTextStream out(&file2);
+
+    while (!in.atEnd()) {
+        QString line = in.readLine();
+
+        if (line == "")
+            spaces_counter++;
+
+        if (spaces_counter%3 == 0 && line != "")
+            category = line;
+
+        if (spaces_counter%3 == 1 && line != "" && category == "Cocktails"){
+            if (substring == ""){
+                List->append(line [0].toUpper()
+                        + line.mid(1, line.size () - 1).toLower ());
+            }
+            else {
+                if (line.toUpper().startsWith(substring.toUpper()))
+                    List->append(line [0].toUpper()
+                            + line.mid(1, line.size () - 1).toLower ());
+            }
+        }
+    }
+
+
+
+    file.close();
+
+}
