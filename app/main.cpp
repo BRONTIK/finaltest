28f290325982a7923775256af9970977f03ab59a
BRONTIK <50723771+BRONTIK@users.noreply.github.com>
2019-06-03 00:16:01 +0300

    Update main.cpp

--- app/main.cpp
+++ app/main.cpp
@@ -1,22 +1,6 @@
-// #include "widget.h"
-// #include <QApplication>
-// #include <QListWidget>
-// #include <QListWidgetItem>
-// #include <QString>
-// #include <QDir>
-// #include <QFile>
-// #include <QStringList>
-// #include <string>
-// #include <iostream>
-
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-//     QApplication a(argc, argv);
-//     Widget w;
-//     w.show();
-
-//     return a.exec();
     return 0;
 }
